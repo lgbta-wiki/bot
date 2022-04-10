a84d8374cba2d9f37a2ebdd0dbdd5e3d91d559c8
Cloud <78389093+froggiecloud@users.noreply.github.com>
2022-04-10 12:43:00 -0700

    updating tester commands

--- src/commands/fun/ping.ts
+++ src/commands/fun/ping.ts
@@ -2,32 +2,28 @@ import { ApplyOptions } from '@sapphire/decorators'
 import { CommandOptions } from '@sapphire/framework'
 import { CommandInteraction } from 'discord.js'
 import RainCommand from '../../structures/RainCommand'
-import { ArgsUser } from '../../types/misc'
 
 @ApplyOptions<CommandOptions>({
-	name: 'pronouns',
-	aliases: ['pronouns'],
-	description: 'see someones pronouns',
+	name: 'ping',
+	aliases: ['ping'],
+	description: 'Get bot ping',
 	preconditions: ['slashOnly'],
 	slashOptions: {
 		guildIDs: ['931713412256387143'],
 //		idHints: ['962461183493959722'],
-		options: [
-			{
-				name: 'user',
-				description: 'The user you want to know the pronouns of',
-				type: 'STRING',
-				required: true,
-			},
-		],
+		//</CommandOptions>options: [
+			//{
+			//	name: 'user',
+			//	description: 'The user you want to know the pronouns of',
+			//	type: 'STRING',
+			//	required: true,
+			//},
+	//	],
 	},
 })
 export class PronounsCommand extends RainCommand {
 	public override async chatInputRun(interaction: CommandInteraction) {
-		const args: { user: ArgsUser } = this.container.utils.parseInteractionArgs(interaction)
 
-		const pronouns = await this.container.utils.getPronouns(args.user.user)
-
-		await interaction.reply(`${pronouns}`)
+		await interaction.reply(`Pong`)
 	}
 }
